c315a009e243cb5cc0929ec4035796687a181f30
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-05 12:46:48 +0100

    feat: genre enum class initialised

--- src/main/java/com/example/RecordShop/model/Genre.java
+++ src/main/java/com/example/RecordShop/model/Genre.java
@@ -1,2 +1,18 @@
-package com.example.RecordShop.model;public enum Genre {
+package com.example.RecordShop.model;
+
+public enum Genre {
+    AFROBEATS("Afrobeats"),
+    POP("Pop"),
+    SOCA("Soca"),
+    JAZZ("Jazz"),
+    BLUES("Blues"),
+    HIPHOP("Hip-Hop"),
+    SALSA("Salsa"),
+    RANDB("R&B");
+
+    String genre;
+
+    Genre(String genre) {
+        this.genre = genre;
+    }
 }
