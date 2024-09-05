5b9cd5ead634a46e6b1d8bbfbe20b6de28c1c653
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-05 15:46:27 +0100

    refactor: genre  model reflecting postgresql database for production environment

--- src/main/java/com/example/RecordShop/model/Genre.java
+++ src/main/java/com/example/RecordShop/model/Genre.java
@@ -1,5 +1,13 @@
 package com.example.RecordShop.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public enum Genre {
         AFROBEATS("Afrobeats"),
         SOCA("Soca"),
@@ -10,6 +18,15 @@ public enum Genre {
         BLUES("Blues"),
         LATINALTERNATIVE("Latin Alternative");
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    Long genre_id;
+
+    @OneToOne(mappedBy = "album", fetch = FetchType.LAZY)
+    Album album;
+
+    @Column(name = "name")
     String genre;
 
     Genre(String genre) {
