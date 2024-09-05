058bb0ad8b1b10f4acb90b4d004a98ba2c6ae3ac
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-05 12:22:26 +0100

    feat: artist data model initialised

--- src/main/java/com/example/RecordShop/model/Artist.java
+++ src/main/java/com/example/RecordShop/model/Artist.java
@@ -1,2 +1,39 @@
-package com.example.RecordShop.model;public class Artist {
+package com.example.RecordShop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    int age;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Column
+    private Date dateOfBirth;
+
+    @Column
+    private String placeOfBirth;
+
+    @OneToMany(mappedBy = "artist", fetch = FetchType.LAZY)
+    private Set<Album> albumList;
+
 }
