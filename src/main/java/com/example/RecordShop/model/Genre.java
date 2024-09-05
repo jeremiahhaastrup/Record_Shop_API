5d02c558960a3b5d8ea9e0a1e322b55cc91bc385
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-05 12:23:06 +0100

    feat: album genre enum class initialised

--- src/main/java/com/example/RecordShop/model/Genre.java
+++ src/main/java/com/example/RecordShop/model/Genre.java
@@ -1,2 +1,18 @@
-package com.example.RecordShop.model;public enum Genre {
+package com.example.RecordShop.model;
+
+public enum Genre {
+        AFROBEATS("Afrobeats"),
+        SOCA("Soca"),
+        RANDB("R&B"),
+        HIPHOP("Hip-Hop"),
+        JAZZ("Jazz"),
+        SALSA("Salsa"),
+        BLUES("Blues"),
+        LATINALTERNATIVE("Latin Alternative");
+
+    String genre;
+
+    Genre(String genre) {
+        this.genre = genre;
+    }
 }
