49c2691d3cc69f87fe40dc0a818474bee7b6f8bb
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-05 15:48:39 +0100

    feat: artist repository layer implemented

--- src/main/java/com/example/RecordShop/repository/ArtistRepository.java
+++ src/main/java/com/example/RecordShop/repository/ArtistRepository.java
@@ -1,2 +1,7 @@
-package com.example.RecordShop.repository;public interface ArtistRepository {
+package com.example.RecordShop.repository;
+
+import com.example.RecordShop.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtistRepository extends CrudRepository<Artist,Long> {
 }
