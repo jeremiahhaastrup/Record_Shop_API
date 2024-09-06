1ea352857ea052a5c4e89ebb582856fabb2102cc
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-06 10:25:35 +0100

    feat: get all albums method for business logic implemented

--- src/main/java/com/example/RecordShop/service/AlbumServiceImpl.java
+++ src/main/java/com/example/RecordShop/service/AlbumServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.RecordShop.service;
 
+import com.example.RecordShop.model.Album;
+import com.example.RecordShop.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class AlbumServiceImpl implements AlbumService {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
+    }
 }
