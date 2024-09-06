8fe68a12eb8f85e3671b339781ed68606cf39791
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-06 10:22:28 +0100

    test: GET /albums test implemented

---
+++ src/main/java/com/example/RecordShop/controller/ArtistController.java
@@ -0,0 +1,2 @@
+package com.example.RecordShop.controller;public class ArtistController {
+}
---
+++ src/test/java/com/example/RecordShop/service/AlbumServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.example.RecordShop.service;
+
+import com.example.RecordShop.model.Album;
+import com.example.RecordShop.model.Artist;
+import com.example.RecordShop.model.Genre;
+import com.example.RecordShop.repository.AlbumRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AlbumServiceImplTest {
+
+    @Mock
+    private AlbumRepository mockAlbumRepository;
+
+    @InjectMocks
+    private AlbumServiceImpl albumServiceImpl;
+
+    @Test
+    @DisplayName("GET /albums")
+    void getAllAlbums() {
+
+        Artist frankOcean = Artist.builder().artist_id(1L).name("Frank Ocean").placeOfBirth("Long Beach, California, USA").dateOfBirth("28-10-1987").build();
+        Artist kendrickLamar = Artist.builder().artist_id(10L).name("Kendrick Lamar").placeOfBirth("Compton, California, USA").dateOfBirth("17-06-1987").build();
+
+        List<Album> expected = List.of(
+                new Album(1L, "Soca Gold 2018", 200, 2500, LocalDate.of(2022, 8, 15), Genre.AFROBEATS, frankOcean),
+                new Album(2L, "To Pimp a Butterfly", 150, 2300, LocalDate.of(2023, 4, 12), Genre.HIPHOP, kendrickLamar)
+        );
+
+        when(mockAlbumRepository.findAll()).thenReturn(expected);
+
+        List<Album> actual = albumServiceImpl.getAllAlbums();
+
+        assertIterableEquals(actual, expected);
+    }
+}
