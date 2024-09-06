0901b09fb843c0c7ec8be43574115d70419ae666
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-06 15:55:17 +0100

    feat: global exception handler implemented

--- src/main/java/com/example/RecordShop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/RecordShop/exception/GlobalExceptionHandler.java
@@ -2,13 +2,30 @@ package com.example.RecordShop.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler({NoSuchAlbumException.class})
+    public ResponseEntity<Object> handleAlbumNotFoundException(NoSuchAlbumException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({AlbumAlreadyExistsException.class})
+    public ResponseEntity<Object> handleAlbumAlreadyExistsException(AlbumAlreadyExistsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
 }
