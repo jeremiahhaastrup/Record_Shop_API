bd47e4cb8bbf12f53e7bceed91677dc100e01c7d
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-06 13:37:26 +0100

    feat: addAlbum method for POST mapping implemented

--- src/main/java/com/example/RecordShop/controller/AlbumController.java
+++ src/main/java/com/example/RecordShop/controller/AlbumController.java
@@ -4,13 +4,8 @@ import com.example.RecordShop.model.Album;
 import com.example.RecordShop.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +18,11 @@ public class AlbumController {
     @GetMapping
     public ResponseEntity<List<Album>> getAllAlbums() {
         return new ResponseEntity<>(albumService.getAllAlbums(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Album> addAlbum(@RequestBody Album album) {
+        Album addAlbum = albumService.addAlbum(album);
+        return new ResponseEntity<>(addAlbum, HttpStatus.CREATED);
     }
 }
