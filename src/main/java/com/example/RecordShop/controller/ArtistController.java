a388c0c91a6851c7ee627f9c1f2e1ad1b5166964
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-07 11:40:19 +0100

    feat: GET artist by id controller method

--- src/main/java/com/example/RecordShop/controller/ArtistController.java
+++ src/main/java/com/example/RecordShop/controller/ArtistController.java
@@ -1,25 +1,36 @@
 package com.example.RecordShop.controller;
 
+import com.example.RecordShop.model.Album;
 import com.example.RecordShop.model.Artist;
 import com.example.RecordShop.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/artists")
 public class ArtistController {
 
     @Autowired
     ArtistService artistService;
 
-        @GetMapping
-        public ResponseEntity<List<Artist>> getAllAlbums() {
-            return new ResponseEntity<>(artistService.getAllArtists(), HttpStatus.OK);
-        }
+    @GetMapping
+    public ResponseEntity<List<Artist>> getAllAlbums() {
+        return new ResponseEntity<>(artistService.getAllArtists(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Artist> addArtist(@RequestBody Artist artist) {
+        Artist addArtist = artistService.addArtist(artist);
+        return new ResponseEntity<>(addArtist, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artist> getArtistById(@PathVariable Long id) {
+        Artist artist = artistService.getArtistById(id);
+        return new ResponseEntity<>(artist, HttpStatus.OK);
+    }
 }
