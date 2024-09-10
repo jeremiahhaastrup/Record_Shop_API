5803cdf070c792b4cbd48dbaea9709aa52c8133b
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-10 18:20:36 +0100

    refactor: CRUDRepository to JpaRepository

--- src/main/java/com/example/RecordShop/repository/AlbumRepository.java
+++ src/main/java/com/example/RecordShop/repository/AlbumRepository.java
@@ -1,9 +1,9 @@
 package com.example.RecordShop.repository;
 
 import com.example.RecordShop.model.Album;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlbumRepository extends CrudRepository <Album, Long> {
+public interface AlbumRepository extends JpaRepository <Album, Long> {
 }
