382e5178f307f3b2d3e2ad42f3d57dee5bfab629
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-10 17:11:55 +0100

    test: GET albums in stock for service layer

--- src/test/java/com/example/RecordShop/service/AlbumServiceImplTest.java
+++ src/test/java/com/example/RecordShop/service/AlbumServiceImplTest.java
@@ -51,6 +51,25 @@ class AlbumServiceImplTest {
     }
 
     @Test
+    @DisplayName("GET /albums in stock")
+    void testGetAllAlbumsInStock() {
+
+        Artist frankOcean = Artist.builder().artist_id(1L).name("Frank Ocean").placeOfBirth("Long Beach, California, USA").dateOfBirth("28/10/1987").build();
+        Artist kendrickLamar = Artist.builder().artist_id(10L).name("Kendrick Lamar").placeOfBirth("Compton, California, USA").dateOfBirth("17/06/1987").build();
+
+        List<Album> expected = List.of(
+                new Album(1L, "Soca Gold 2018", 200, 2500, LocalDate.of(2022, 8, 15), Genre.AFROBEATS, frankOcean),
+                new Album(2L, "To Pimp a Butterfly", 150, 2300, LocalDate.of(2023, 4, 12), Genre.HIPHOP, kendrickLamar)
+        );
+
+        when(mockAlbumRepository.findAll()).thenReturn(expected);
+
+        List<Album> actual = albumServiceImpl.getAllAlbumsInStock();
+
+        assertIterableEquals(actual, expected);
+    }
+
+    @Test
     @DisplayName("POST /albums")
     void testPostAlbum() {
 
