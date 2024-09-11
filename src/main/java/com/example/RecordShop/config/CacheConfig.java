f5109377a8c92cdd8c41f09a345d18201045b051
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-11 15:23:09 +0100

    feat: cache configuration with caffiene

---
+++ src/main/java/com/example/RecordShop/config/CacheConfig.java
@@ -0,0 +1,32 @@
+package com.example.RecordShop.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("AlbumCache");
+        caffeineCacheManager.setCaffeine(caffeineConfig());
+        return caffeineCacheManager;
+    }
+}
