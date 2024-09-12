ad25cc43520f926582091510fa911e78f95d2037
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-12 09:02:18 +0100

    feat: swagger ui implementation

--- src/main/java/com/example/RecordShop/RecordShopApplication.java
+++ src/main/java/com/example/RecordShop/RecordShopApplication.java
@@ -1,13 +1,28 @@
 package com.example.RecordShop;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class RecordShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecordShopApplication.class, args);
 	}
+
+		@Bean
+		public OpenAPI todoApiInfo() {
+			return new OpenAPI()
+					.info(new Info().title("Welcome to the Record Shop")
+							.description("Need an Album or Artist? This is the API for you! \uD83D\uDE3A")
+							.version("v1.1")
+							.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+		}
 
 }
