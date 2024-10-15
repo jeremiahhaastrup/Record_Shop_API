b880209595f6d946b1b0093f56b25e4f2b87c2b8
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-15 13:59:51 +0100

    feat: more genres and formatted genre names

--- src/main/java/com/example/RecordShop/type/Genre.java
+++ src/main/java/com/example/RecordShop/type/Genre.java
@@ -1,13 +1,69 @@
 package com.example.RecordShop.type;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Genre {
     AFROBEATS,
     SOCA,
-    RANDB,
+    R_AND_B,
     HIPHOP,
     JAZZ,
     SALSA,
     BLUES,
-    LATIN_ALTERNATIVE;
+    LATIN_ALTERNATIVE,
+    ROCK,
+    POP,
+    CLASSICAL,
+    REGGAE,
+    DANCEHALL,
+    METAL,
+    COUNTRY,
+    FOLK,
+    ELECTRONIC,
+    HOUSE,
+    TECHNO,
+    DISCO,
+    GOSPEL,
+    FUNK,
+    PUNK,
+    INDIE,
+    GRUNGE,
+    KPOP,
+    TRAP,
+    DRUM_AND_BASS,
+    AMBIENT,
+    DUBSTEP,
+    EDM,
+    SYNTHPOP,
+    OPERA,
+    SWING,
+    TRANCE,
+    SKA,
+    WORLD_MUSIC,
+    NEW_WAVE,
+    ALTERNATIVE_ROCK,
+    PSYCHEDELIC,
+    PROGRESSIVE_ROCK,
+    POST_ROCK,
+    LOFI_HIP_HOP,
+    CUMBIA,
+    BHANGRA;
 
+    @JsonValue
+    public String getFormattedGenre() {
+        String genre = this.name().toLowerCase();
+        genre = genre.replace("_AND_", "&");
+        genre = genre.replace("_", " ");
+
+        String[] genres = genre.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : genres) {
+            result.append(Character.toUpperCase(word.charAt(0)))
+                    .append(word.substring(1))
+                    .append(" ");
+        }
+
+        return result.toString().trim();
+    }
 }
