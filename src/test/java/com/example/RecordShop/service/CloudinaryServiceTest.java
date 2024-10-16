98dd7d5cc9e4534901bae59582e9dc6c9208d23a
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-16 13:12:33 +0100

    test: cloudinary response issue implemented

--- src/test/java/com/example/RecordShop/service/CloudinaryServiceTest.java
+++ src/test/java/com/example/RecordShop/service/CloudinaryServiceTest.java
@@ -11,8 +11,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.when;
 
 class CloudinaryServiceTest {
@@ -39,5 +42,12 @@ class CloudinaryServiceTest {
         assertThrows(IOException.class, () -> cloudinaryService.uploadImage(mockFile));
     }
 
+    @Test
+    @DisplayName("Cloudinary Response Issue")
+    void ImageUploadCloudinaryResponse() throws IOException {
+        MockMultipartFile mockFile = new MockMultipartFile("image", "originalFilename.png", "image/png", "file content".getBytes());
+        when(cloudinary.uploader().upload(any(byte[].class), anyMap())).thenReturn(new HashMap<>());
+        assertThrows(IOException.class, () -> cloudinaryService.uploadImage(mockFile));
+    }
 
 }
