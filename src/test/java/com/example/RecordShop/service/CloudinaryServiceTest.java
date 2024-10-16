4d97294ec02248253359437a8fa1984f5e27a7a7
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-16 12:50:47 +0100

    test: empty file uploaded test implemented

---
+++ src/test/java/com/example/RecordShop/service/CloudinaryServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.RecordShop.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class CloudinaryServiceTest {
+
+    @Mock
+    Cloudinary cloudinary;
+
+    @Mock
+    Uploader uploader;
+
+    @InjectMocks
+    CloudinaryService cloudinaryService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        when(cloudinary.uploader()).thenReturn(uploader);
+    }
+
+    @Test
+    @DisplayName("Empty File Uploaded")
+    void ImageUploadEmptyFile() throws IOException {
+        MockMultipartFile mockFile = new MockMultipartFile("image", "originalFilename.png", "image/png", new byte[0]);
+        assertThrows(IOException.class, () -> cloudinaryService.uploadImage(mockFile));
+    }
+
+
+}
