12ca5bb77636a24d7fdc151adbb1f96f3bca6a5e
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-16 15:45:46 +0100

    refactor: custom global exceptions implemented

--- src/main/java/com/example/RecordShop/service/CloudinaryService.java
+++ src/main/java/com/example/RecordShop/service/CloudinaryService.java
@@ -2,6 +2,8 @@ package com.example.RecordShop.service;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import com.example.RecordShop.exception.FailedImageUploadException;
+import com.example.RecordShop.exception.ImageFileEmptyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +23,7 @@ public class CloudinaryService {
 
     public String uploadImage(MultipartFile file) throws IOException {
         if (file == null || file.isEmpty()) {
-            throw new IOException("File is empty");
+            throw new ImageFileEmptyException("File is empty");
         }
 
         Map params = ObjectUtils.asMap(
@@ -33,11 +35,11 @@ public class CloudinaryService {
         try {
             Map result = cloudinary.uploader().upload(file.getBytes(), params);
             if (result == null || !result.containsKey("secure_url") || result.get("secure_url") == null) {
-                throw new IOException("Failed to retrieve secure URL from cloudinary response");
+                throw new FailedImageUploadException("Failed to retrieve secure URL from cloudinary response");
             }
             return (String) result.get("secure_url");
         } catch (RuntimeException e) {
-            throw new IOException("Upload failed to Cloudinary: " + e.getMessage(), e);
+            throw new FailedImageUploadException("Upload failed to Cloudinary");
         }
     }
 }
